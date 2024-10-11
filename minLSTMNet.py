ecb6883c754eda398f96c1e0cc2f4287130d84cf
Axion66 <ssongjinseob@gmail.com>
2024-10-11 15:14:14 +0900

    only parallel mode is now supported. fixed output shape mismatch batch,seq+1,out_chn to batch,seq,out_chn

--- minLSTMNet.py
+++ minLSTMNet.py
@@ -2,117 +2,44 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def g(x):
-    return torch.where(x >= 0, x+0.5, torch.sigmoid(x))
 
-def log_g(x):
-    return torch.where(x >= 0, (F.relu(x)+0.5).log(),-F.softplus(-x))
+class MinLSTM(nn.Module):
+    """
+        Only "parallel mode" is supported for conciseness.
+        use log space.
 
-def parallel_scan_log(log_coeffs, log_values):
-    a_star = F.pad(torch.cumsum(log_coeffs, dim=1), (0, 0, 1, 0)).squeeze(1)
-    log_h0_plus_b_star = torch.logcumsumexp(log_values - a_star, dim=1).squeeze(1)
-    log_h = a_star + log_h0_plus_b_star
-    return torch.exp(log_h)
-
-class MinLSTMCell(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, mode='seq', device=None, dtype=None):
+        input shape: [batch, seq_len, in_chn]
+        output shape: [batch,seq_len, out_chn]
+    """
+    def __init__(self, input_size: int, hidden_size: int, device=None, dtype=None):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        
-        self.linear_f = nn.Linear(input_size, hidden_size, device=device, dtype=dtype)
-        self.linear_i = nn.Linear(input_size, hidden_size, device=device, dtype=dtype)
-        self.linear_h = nn.Linear(input_size, hidden_size, device=device, dtype=dtype)
+        self.linear = nn.Linear(input_size, hidden_size*3,bias=False,device=device, dtype=dtype)
 
-        self.mode = mode
 
 
     def forward(self, x_t, h_prev=None):
-        
-        if h_prev is None:
-            h_prev = torch.zeros(x_t.size(0), self.hidden_size, device=x_t.device, dtype=x_t.dtype)
-        
-        if self.mode == 'seq':
-            is_batched = x_t.dim() == 2
-            if not is_batched:
-                x_t = x_t.unsqueeze(0)
-            f_t = torch.sigmoid(self.linear_f(x_t))
-            i_t = torch.sigmoid(self.linear_i(x_t))
-            
-            h_tilde = g(self.linear_h(x_t))
+        seq_len = x_t.shape[1]
+        f,i,h = torch.chunk(self.linear(x_t),chunks=3,dim=-1)
+        diff = F.softplus(-f) - F.softplus(-i)
+        log_f = -F.softplus(diff)
+        log_i = -F.softplus(-diff)
+        log_h_0 = self.log_g(h_prev)
+        log_tilde_h = self.log_g(h)
+        log_coeff = log_f.unsqueeze(1)
+        log_val = torch.cat([log_h_0.unsqueeze(1), (log_i + log_tilde_h)], dim=1)
+        h_t = self.parallel_scan_log(log_coeff,log_val)
+        return h_t[:,-seq_len:]
 
-            f_prime_t = f_t / (f_t + i_t)
-            i_prime_t = i_t / (f_t + i_t)
-            h_t = f_prime_t * h_prev + i_prime_t * h_tilde
+    def parallel_scan_log(log_coeffs, log_values):
+        a_star = F.pad(torch.cumsum(log_coeffs, dim=1), (0, 0, 1, 0)).squeeze(1)
+        log_h0_plus_b_star = torch.logcumsumexp(log_values - a_star, dim=1).squeeze(1)
+        log_h = a_star + log_h0_plus_b_star
+        return torch.exp(log_h) # will return [batch, seq + 1, chn]
 
-            if not is_batched:
-                h_t = h_t.squeeze(0)
-        elif self.mode == 'par':
-            diff = F.softplus(-self.linear_f(x_t)) - F.softplus(-self.linear_i(x_t))
-            log_f = -F.softplus(diff)
-            log_i = -F.softplus(-diff)
-            log_h_0 = log_g(h_prev)
-            log_tilde_h = log_g(self.linear_h(x_t))
+    def g(self,x):
+        return torch.where(x >= 0, x+0.5, torch.sigmoid(x))
 
-            log_coeff = log_f.unsqueeze(1)
-            log_val = torch.cat([log_h_0.unsqueeze(1), (log_i + log_tilde_h)], dim=1)
-            h_t = parallel_scan_log(log_coeff,log_val)
-        else:
-            raise ValueError(f"Expected mode 'seq' or 'par', but got {self.mode}") 
-        
-        
-        return h_t
-
-class MinLSTM(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, mode="par", 
-                 batch_first: bool = False, bidirectional: bool = False, device=None, dtype=None):
-        
-        super().__init__()
-        self.mode = mode
-        self.mini_lstm_fwd = MinLSTMCell(input_size, hidden_size, mode, device=device, dtype=dtype)
-        self.bidirectional = bidirectional
-        if bidirectional:
-            self.mini_lstm_bwd = MinLSTMCell(input_size, hidden_size, mode, device=device, dtype=dtype)
-        self.batch_first = batch_first
-
-    def forward(self, x, h_0=None):
-        if self.batch_first:
-            x = x.transpose(0, 1)
-        
-        seq_len, batch_size, _ = x.size()
-        hidden_size = self.mini_lstm_fwd.hidden_size
-        
-
-        if self.mode == 'seq':
-            if h_0 is None:
-                h_0 = torch.zeros(batch_size, hidden_size, device=x.device, dtype=x.dtype)
-            
-            output_fwd = []
-            h_t_fwd = h_0
-            
-            for t in range(seq_len):
-                h_t_fwd = self.mini_lstm_fwd(x[t], h_t_fwd)
-                output_fwd.append(h_t_fwd)
-            
-            output_fwd = torch.stack(output_fwd, dim=0)
-            
-            if self.bidirectional:
-                output_bwd = []
-                h_t_bwd = h_0
-                for t in reversed(range(seq_len)):
-                    h_t_bwd = self.mini_lstm_bwd(x[t], h_t_bwd)
-                    output_bwd.append(self.dropout(h_t_bwd))
-                output_bwd = torch.stack(output_bwd[::-1], dim=0)
-                output = torch.cat([output_fwd, output_bwd], dim=2)
-            else:
-                output = output_fwd
-
-            if self.batch_first:
-                output = output.transpose(0, 1)
-            
-            return output, h_t_fwd
-        elif self.mode == 'par':
-            output = self.mini_lstm_fwd(x)
-            return output,output
-        else:
-            raise ValueError(f"Expected mode 'seq' or 'par', but got {self.mode}") 
+    def log_g(self,x):
+        return torch.where(x >= 0, (F.relu(x)+0.5).log(),-F.softplus(-x))
